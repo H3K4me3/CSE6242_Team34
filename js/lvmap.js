533aa3a312e0849684fcd61980f49e8221c6f9b2
Jialin Ma <marlin-@gmx.cn>
2018-10-31 06:52:25 -0400

    Draw circle layer

--- js/lvmap.js
+++ js/lvmap.js
@@ -3,3 +3,40 @@ L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',{
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
 }).addTo(leafletMap);
+
+
+var svgMap = d3.select(leafletMap.getPanes().overlayPane).append("svg")
+  .attr("width", 2000).attr("height", 2500);
+var circleGroup = svgMap.append("g").attr("class", "leaflet-zoom-hide");
+
+
+d3.json("data/yelp_LasVegas_business.json", function(error, dataset) {
+    if (error)
+        throw error;
+
+    // Modification on the data
+    dataset = dataset.filter(function(d) {
+        // There is one weird entry that does not
+        // have longitude
+        return !isNaN(d.longitude) & !isNaN(d.latitude);
+    })
+    dataset.forEach(function(d) {
+        d.LatLng = new L.LatLng(d.latitude, d.longitude);
+    })
+
+    function draw_circle() {
+        var circleSel = circleGroup.selectAll("circle")
+            .data(dataset);
+        circleSel.enter()
+            .append("circle")
+            .attr("pointer-events", "visible")
+            .attr("class", "circleMap")
+            .attr("r", 7)
+          .merge(circleSel) // Enter + Update
+            .attr("cx", d => leafletMap.latLngToLayerPoint(d.LatLng).x)
+            .attr("cy", d => leafletMap.latLngToLayerPoint(d.LatLng).y);
+    }
+    leafletMap.on("viewreset", draw_circle);
+    draw_circle(dataset);
+})
+
